80ee0ba429ed2620c34a98d9b34f3681886a4dc8
saad <saaidisaad2002@gmail.com>
2025-11-12 11:58:40 +0100

    Initial commit du projet tp_JEE

---
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,47 @@
+package com.example.customerservice;
+
+import com.example.customerservice.repository.CustomerRepository;
+import com.example.customerservice.entities.Customer; // make sure this import matches your package
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(
+                    Customer.builder()
+                            .name("Mohamed")
+                            .email("med@gmail.com")
+                            .build()
+            );
+            customerRepository.save(
+                    Customer.builder()
+                            .name("imane")
+                            .email("imane@gmail.com")
+                            .build()
+            );
+            customerRepository.save(
+                    Customer.builder()
+                            .name("yassine")
+                            .email("yassine@gmail.com")
+                            .build()
+            );
+            customerRepository.findAll().forEach(c->{
+                System.out.println("=====================");
+                System.out.println(c.getId());
+                System.out.println(c.getName());
+                System.out.println(c.getEmail());
+                System.out.println("==============");
+            });
+        };
+    }
+}
